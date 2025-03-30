8e4a6d878b5a82fac525408f26563ca5ab4d75e7
Gastón Gadea <53276272+gastongadea@users.noreply.github.com>
2025-03-30 20:21:29 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,11 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full1=f'{first_name.lower()} {last_name.lower()}'
+    full2=f'{first_name.title()} {last_name.title()}'
+    full3=f'{first_name.upper()} {last_name.upper()}'
+    full4=f'\t{full1}'
+    print (full1)
+    print (full2)
+    print (full3)
+    print (full4)
