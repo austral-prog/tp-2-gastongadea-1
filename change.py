4dd379c1615bd5b84a7f86e9be0c92c6b68306b8
Gastón Gadea <53276272+gastongadea@users.noreply.github.com>
2025-03-30 20:39:30 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,16 @@
 def change():
     expense = 23.75
     money = 100
+    print('Ingresar gasto:')
+    print(expense)
+    print('Dinero recibido')
+    print(money)
+    print()
+    print('Vuelto')
+    print()
+    print('Pesos:')
+    vuelto=money-expense
+    print(vuelto)
+    print('Centavos:')
+    cents=int((vuelto-int(vuelto))*100)
+    print(cents)
